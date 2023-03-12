c4d8ee8c42342c36ef4d8dba5511bfb09a70066f
avivashishta29 <avivashishta@gmail.com>
2023-03-12 12:48:48 +0530

    contest1

---
+++ contest1.js
@@ -0,0 +1,12 @@
+var color = prompt("Whats your fav colour");
+if (color == null) {
+  //   something
+} else {
+  color = color.toLowerCase();
+  console.log(color);
+  if (color == "red") {
+    // increase score of red
+  } else if (color == "yellow") {
+    // increase score of yellow
+  }
+}
