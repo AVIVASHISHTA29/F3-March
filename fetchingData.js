53fa36a5734b8968cdbc7e876bffea6e43d9ae3f
avivashishta29 <avivashishta@gmail.com>
2023-03-13 23:00:38 +0530

    fetching data

---
+++ fetchingData.js
@@ -0,0 +1,44 @@
+console.log("fetching Data");
+// Fetch is an inbuilt method provided by the BROWSER which returns a promise.
+
+// FETCH is this method that we use to receive data from the url that we pass in the param
+
+// FETCH  returns a RESPONSE object.
+
+// The response.json() method reads the data returned by the server and returns a Promise.
+
+// fetch("https://dummyjson.com/quotes")
+//   .then((response) => response.json())
+//   .then((data) => {
+// DO WHATEVER YOU WANT TO WITH THAT DATA
+// });
+
+// fetch("https://dummyjson.com/quotes")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("DATA>>>", data);
+//     let myArr = data.quotes;
+//     let myDiv = document.getElementById("results");
+//     myArr.map((quote) => {
+//       myDiv.innerHTML += `<p><strong>${quote.id} - ${quote.author} </strong> ${quote.quote}</p>`;
+//     });
+//   });
+
+fetch("https://dummyjson.com/quotes")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Data", data);
+    const quotes = data.quotes;
+    console.log("My Quotes", quotes);
+    const myHTML = quotes.map((item) => {
+      return `
+        <div class='quote-box'>
+            <h2>${item.id}. ${item.author}</h2>
+            <p>${item.quote}</p>
+        </div>
+      `;
+    });
+    document.getElementById("container").innerHTML = myHTML.join(" ");
+  });
+
+console.log("still fetching Data...");
