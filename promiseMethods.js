86801f9e55af0fdd12101e34a8d800020e970511
avivashishta29 <avivashishta@gmail.com>
2023-03-14 22:41:38 +0530

    promise methods

--- promiseMethods.js
+++ promiseMethods.js
@@ -2,20 +2,20 @@ console.log("Promise Methods");
 
 let promise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject("Promise1");
+    resolve("Promise1");
   }, 1000);
 });
 
 let promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject("Could not resolve promise 2");
-    // resolve("Promise 2");
+    // reject("Could not resolve promise 2");
+    resolve("Promise 2");
   }, 2000);
 });
 
 let promise3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject("Promise 3");
+    resolve("Promise 3");
   }, 3000);
 });
 
@@ -109,7 +109,7 @@ let promise3 = new Promise((resolve, reject) => {
 
 const myArray = [promise1, promise2, promise3];
 
-Promise.any(myArray)
+Promise.all(myArray)
   .then((res) => {
     console.log("ANY", res);
   })
