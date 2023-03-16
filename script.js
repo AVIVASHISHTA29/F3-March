0088d8a30509e3fd7d324b6958f515c79534491c
avivashishta29 <avivashishta@gmail.com>
2023-03-16 22:33:03 +0530

    event loop

---
+++ script.js
@@ -0,0 +1,35 @@
+// function sayHello2() {
+//   console.log("Timeout2");
+// }
+
+// setTimeout(sayHello, 1000);
+// setTimeout(sayHello2, 500);
+// console.log("Hello world 1");
+// console.log("Hello world 2");
+// console.log("Hello world 3");
+
+// console.log("Hi");
+// document.getElementById("btn")
+// .addEventListener("click", BtnClicked);
+// console.log("Bye");
+
+// function BtnClicked() {
+//   console.log("clicked!");
+// }
+
+// fetch("https://dummyjson.com/quotes/1")
+//   .then((res) => res.json())
+//   .then((data) => dataConsole(data));
+
+// function sayHello() {
+//   console.log("Timeout");
+// }
+
+// function dataConsole(data) {
+//   console.log(data);
+// }
+
+// setTimeout(sayHello, 1000);
+
+var x = 100;
+var myString = "hi hello how are you";
